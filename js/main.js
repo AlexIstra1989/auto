da0c37868e2b675c8cbab7ae09ab117958a64ab2
AlexIstra1989 <alex888fcsm@gmail.com>
2022-05-01 20:32:45 +0300

    added adaptive

--- js/main.js
+++ js/main.js
@@ -4,5 +4,19 @@ $(document).ready(function(){
   dots: true,
   slidesToShow: 3,
   slidesToScroll: 2,
+  responsive: [
+   {
+    breakpoint: 768,
+     settings: {
+      slidesToShow: 2
+     }
+   },
+   {
+    breakpoint: 600,
+     settings: {
+      slidesToShow: 1
+     }
+   },
+  ]
  });
 });
